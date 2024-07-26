552a548105339271a79948099948ca0bbbe5b7ef
Naoki Kuzuya <33458465+coyote009@users.noreply.github.com>
2024-07-26 11:33:55 +0900

    Camera intrinsic calibration by python

--- python_calib_intrinsic/calib_intrinsic.py
+++ python_calib_intrinsic/calib_intrinsic.py
@@ -16,62 +16,68 @@ def calibrate(img_points, board_size, img_size):
 
     return rms_err, mat_cam, vec_dist
 
-img_size = (640, 480)
-board_size = (6, 5)
+def calib_intrinsic(img_size, board_size, fname_calib):
 
-camera = Picamera2()
-camera.configure(camera.create_preview_configuration(main={"format": "RGB888",
-                                                           "size": img_size}))
-camera.start()
+    camera = Picamera2()
+    camera.configure(camera.create_preview_configuration(main={"format": "RGB888",
+                                                               "size": img_size}))
+    camera.start()
 
-corner_points = []
-while True:
-    img = camera.capture_array()
+    corner_points = []
+    while True:
+        img = camera.capture_array()
 
-    img_show = img.copy()
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_show = img.copy()
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    found, corners = cv2.findChessboardCorners(img_gray, board_size)
-    if found:
-        corners = cv2.cornerSubPix(img_gray, corners, (11, 11), (-1, -1),
-                                   (cv2.TERM_CRITERIA_EPS +
-                                    cv2.TERM_CRITERIA_MAX_ITER,
-                                    30, 0.1))
+        found, corners = cv2.findChessboardCorners(img_gray, board_size)
+        if found:
+            corners = cv2.cornerSubPix(img_gray, corners, (11, 11), (-1, -1),
+                                       (cv2.TERM_CRITERIA_EPS +
+                                        cv2.TERM_CRITERIA_MAX_ITER,
+                                        30, 0.1))
 
-        cv2.drawChessboardCorners(img_show, board_size, corners, found)
+            cv2.drawChessboardCorners(img_show, board_size, corners, found)
 
-    cv2.imshow("img", img_show)
-
-    key = cv2.waitKey(1)
-    if key == ord(" "):
-        if not found:
-            continue
-
-        cv2.putText(img_show, "Use this image (y)?", (15, 15),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
         cv2.imshow("img", img_show)
-        key = cv2.waitKey()
-        if key != ord("y"):
-            continue
 
-        corner_points += [np.squeeze(corners)]
+        key = cv2.waitKey(1)
+        if key == ord(" "):
+            if not found:
+                continue
 
-    elif key == ord("c"):
-        if len(corner_points) == 0:
-            continue
+            cv2.putText(img_show, "Use this image (y)?", (15, 15),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+            cv2.imshow("img", img_show)
+            key = cv2.waitKey()
+            if key != ord("y"):
+                continue
 
-        img_points = np.array(corner_points)
+            corner_points += [np.squeeze(corners)]
 
-        rms_err, mat_cam, vec_dist = \
-            calibrate(img_points, board_size, img_size)
+        elif key == ord("c"):
+            if len(corner_points) == 0:
+                continue
 
-        np.savez("calib.npz", rms_err=rms_err, mat_cam=mat_cam,
-                 vec_dist=vec_dist)
+            img_points = np.array(corner_points)
 
-        print(f"RMS error = {rms_err}")
-        print(f"Mat Cam = {mat_cam}")
-        print(f"Vec Dist = {vec_dist}")
-        break
-        
-    elif key == 27:
-        break
+            rms_err, mat_cam, vec_dist = \
+                calibrate(img_points, board_size, img_size)
+
+            np.savez(fname_calib, rms_err=rms_err, mat_cam=mat_cam,
+                     vec_dist=vec_dist)
+
+            print(f"RMS error = {rms_err}")
+            print(f"Mat Cam = {mat_cam}")
+            print(f"Vec Dist = {vec_dist}")
+            break
+
+        elif key == 27:
+            break
+
+if __name__ == "__main__":
+    img_size = (640, 480)
+    board_size = (6, 5)
+    fname_calib = "calib.npz"
+    calib_intrinsic(img_size, board_size, fname_calib)
+
