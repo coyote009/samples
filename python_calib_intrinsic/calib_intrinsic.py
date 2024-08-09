062fe03168c8ec41a2f974372cee3a7ef303de11
Naoki Kuzuya <33458465+coyote009@users.noreply.github.com>
2024-08-09 15:00:35 +0900

    Camera intrinsic calibration on Raspberry Pi

--- python_calib_intrinsic/calib_intrinsic.py
+++ python_calib_intrinsic/calib_intrinsic.py
@@ -2,7 +2,8 @@ import numpy as np
 import cv2
 from picamera2 import Picamera2
 
-def calibrate(img_points, board_size, img_size):
+def calibrate(img_points, board_size, img_size, mat_cam=None, vec_dist=None,
+              flags=None):
 
     obj_points = np.zeros((board_size[0]*board_size[1], 3), dtype=np.float32)
     obj_points[:, :2] = np.mgrid[0:board_size[0],
@@ -12,48 +13,52 @@ def calibrate(img_points, board_size, img_size):
     rms_err, \
         mat_cam, vec_dist, \
         rvecs, tvecs = \
-            cv2.calibrateCamera(obj_points, img_points, img_size, None, None)
+            cv2.calibrateCamera(obj_points, img_points, img_size,
+                                mat_cam, vec_dist, flags=flags)
 
     return rms_err, mat_cam, vec_dist
 
-def calib_intrinsic(img_size, board_size, fname_calib):
-
-    camera = Picamera2()
-    camera.configure(camera.create_preview_configuration(main={"format": "RGB888",
-                                                               "size": img_size}))
-    camera.start()
+def calib_intrinsic(camera, img_size, board_size, fname_calib,
+                    mat_cam=None, vec_dist=None, flags=None):
 
     corner_points = []
     while True:
         img = camera.capture_array()
 
         img_show = img.copy()
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        found, corners = cv2.findChessboardCorners(img_gray, board_size)
-        if found:
-            corners = cv2.cornerSubPix(img_gray, corners, (11, 11), (-1, -1),
-                                       (cv2.TERM_CRITERIA_EPS +
-                                        cv2.TERM_CRITERIA_MAX_ITER,
-                                        30, 0.1))
-
-            cv2.drawChessboardCorners(img_show, board_size, corners, found)
-
+        cv2.putText(img_show, f"{len(corner_points)}", (15, 15),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
         cv2.imshow("img", img_show)
 
         key = cv2.waitKey(1)
         if key == ord(" "):
-            if not found:
-                continue
 
-            cv2.putText(img_show, "Use this image (y)?", (15, 15),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-            cv2.imshow("img", img_show)
-            key = cv2.waitKey()
-            if key != ord("y"):
-                continue
+            found = False
+            while not found:
+                img = camera.capture_array()
+                img_show = img.copy()
+                img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-            corner_points += [np.squeeze(corners)]
+                found, corners = cv2.findChessboardCorners(img_gray, board_size)
+                if found:
+                    corners = cv2.cornerSubPix(img_gray, corners, (11, 11), (-1, -1),
+                                               (cv2.TERM_CRITERIA_EPS +
+                                                cv2.TERM_CRITERIA_MAX_ITER,
+                                                30, 0.1))
+
+                    cv2.drawChessboardCorners(img_show, board_size, corners, found)
+
+                cv2.imshow("img", img_show)
+                if cv2.waitKey(1) == 27:
+                    break
+
+            if found:
+                cv2.putText(img_show, "Use this image (y)?", (15, 15),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+                cv2.imshow("img", img_show)
+                key = cv2.waitKey()
+                if key == ord("y"):
+                    corner_points += [np.squeeze(corners)]
 
         elif key == ord("c"):
             if len(corner_points) == 0:
@@ -62,7 +67,8 @@ def calib_intrinsic(img_size, board_size, fname_calib):
             img_points = np.array(corner_points)
 
             rms_err, mat_cam, vec_dist = \
-                calibrate(img_points, board_size, img_size)
+                calibrate(img_points, board_size, img_size, mat_cam, vec_dist,
+                          flags)
 
             np.savez(fname_calib, rms_err=rms_err, mat_cam=mat_cam,
                      vec_dist=vec_dist)
@@ -76,8 +82,13 @@ def calib_intrinsic(img_size, board_size, fname_calib):
             break
 
 if __name__ == "__main__":
+    camera = Picamera2()
+    camera.configure(camera.create_preview_configuration(main={"format": "RGB888",
+                                                               "size": img_size}))
+    camera.start()
+
     img_size = (640, 480)
     board_size = (6, 5)
     fname_calib = "calib.npz"
-    calib_intrinsic(img_size, board_size, fname_calib)
+    calib_intrinsic(camera, img_size, board_size, fname_calib)
 
